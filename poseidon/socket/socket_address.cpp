4f0ab3e6111f6682dc9d2791619935603fbe0fd8
LIU Hao <lh_mouse@126.com>
2023-02-10 00:58:59 +0800

    socket_address: Remove an unnecessary assignment

--- poseidon/socket/socket_address.cpp
+++ poseidon/socket/socket_address.cpp
@@ -157,7 +157,6 @@ parse(stringR str)
     // Break down the host:port string as a URL.
     ::http_parser_url url;
     url.field_set = 0;
-    url.port = 0;
 
     if(::http_parser_parse_url(str.data(), str.size(), true, &url) != 0)
       return false;
