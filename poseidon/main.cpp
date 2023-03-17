0f0371cf02cc90ad243a09af1de5edacfccfef30
LIU Hao <lh_mouse@126.com>
2023-03-17 14:23:20 +0800

    main: Update comments

--- poseidon/main.cpp
+++ poseidon/main.cpp
@@ -273,7 +273,9 @@ do_daemonize_start()
     if(cpid != 0)
       do_await_child_process_and_exit(cpid);
 
-    // The CHILD shall create a new session and becomes its leader.
+    // The CHILD shall create a new session and become its leader. This
+    // ensures that a later GRANDCHILD will not be a session leader and
+    // will not unintentially gain a controlling terminal.
     ::setsid();
 
     int pipefds[2];
