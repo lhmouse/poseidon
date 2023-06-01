lhmouse/poseidon

commit c4742628401326b16f68e40a06e1de041aacb6c2

http_value: Remove redundant checks

poseidon/http/http_value.cpp
@@ -12,11 +12,10 @@ bool
 do_is_ctl_or_sep(char ch) noexcept
   {
     // https://www.rfc-editor.org/rfc/rfc2616#section-2.2
-    return ((ch >= 0x00) && (ch <= 0x1F))
-           || (ch == '(') || (ch == ')') || (ch == '<') || (ch == '>') || (ch == '@')
-           || (ch == ',') || (ch == ';') || (ch == ':') || (ch == '\\') || (ch == '\"')
-           || (ch == '/') || (ch == '[') || (ch == ']') || (ch == '?') || (ch == '=')
-           || (ch == '{') || (ch == '}') || (ch == ' ') || (ch == '\t');
+    return ((ch >= 0x00) && (ch <= 0x1F)) || (ch == '(') || (ch == ')') || (ch == '<')
+           || (ch == '>') || (ch == '@') || (ch == ',') || (ch == ';') || (ch == ':')
+           || (ch == '\\') || (ch == '\"') || (ch == '/') || (ch == '[') || (ch == ']')
+           || (ch == '?') || (ch == '=') || (ch == '{') || (ch == '}');
   }
 
 constexpr

commit a19a3e1c80de0926fc292f1fbb844cce1d11bc88

http_value: Treat spaces as separators
This is a regression from c4742628401326b16f68e40a06e1de041aacb6c2.

poseidon/http/http_value.cpp
@@ -12,7 +12,7 @@ bool
 do_is_ctl_or_sep(char ch) noexcept
   {
     // https://www.rfc-editor.org/rfc/rfc2616#section-2.2
-    return ((ch >= 0x00) && (ch <= 0x1F)) || (ch == '(') || (ch == ')') || (ch == '<')
+    return ((ch >= 0x00) && (ch <= 0x20)) || (ch == '(') || (ch == ')') || (ch == '<')
            || (ch == '>') || (ch == '@') || (ch == ',') || (ch == ';') || (ch == ':')
            || (ch == '\\') || (ch == '\"') || (ch == '/') || (ch == '[') || (ch == ']')
            || (ch == '?') || (ch == '=') || (ch == '{') || (ch == '}');