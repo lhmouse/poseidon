81214adad6e030c47fe08a1369fc0ea39f22c12e
LIU Hao <lh_mouse@126.com>
2023-06-20 13:18:15 +0800

    http_response_headers: New

--- poseidon/http/http_response_headers.cpp
+++ poseidon/http/http_response_headers.cpp
@@ -4,9 +4,44 @@
 #include "../precompiled.ipp"
 #include "http_response_headers.hpp"
 #include "../utils.hpp"
+#include <http_parser.h>
 namespace poseidon {
-namespace {
 
-}  // namespace
+HTTP_Response_Headers::
+~HTTP_Response_Headers()
+  {
+  }
+
+tinyfmt&
+HTTP_Response_Headers::
+print(tinyfmt& fmt) const
+  {
+    // `HTTP/1.1 400 Bad Request`
+    if(this->m_reason.empty())
+      fmt << "HTTP/1.1 " << this->m_status << " " << ::http_status_str((::http_status) this->m_status) << "\r\n";
+    else
+      fmt << "HTTP/1.1 " << this->m_status << " " << this->m_reason << "\r\n";
+
+    // `Server: test`
+    // `Content-Length: 42`
+    for(const auto& r : this->m_headers)
+      if(r.second.is_string())
+        fmt << r.first << ": " << r.second.as_string() << "\r\n";
+      else
+        fmt << r.first << ": " << r.second << "\r\n";
+
+    // Terminate the header with an empty line.
+    fmt << "\r\n";
+    return fmt;
+  }
+
+cow_string
+HTTP_Response_Headers::
+print_to_string() const
+  {
+    ::rocket::tinyfmt_str fmt;
+    this->print(fmt);
+    return fmt.extract_string();
+  }
 
 }  // namespace poseidon
---
+++ poseidon/http/http_response_headers.hpp
@@ -0,0 +1,126 @@
+// This file is part of Poseidon.
+// Copyleft 2022 - 2023, LH_Mouse. All wrongs reserved.
+
+#ifndef POSEIDON_HTTP_HTTP_RESPONSE_HEADERS_
+#define POSEIDON_HTTP_HTTP_RESPONSE_HEADERS_
+
+#include "../fwd.hpp"
+#include "http_value.hpp"
+namespace poseidon {
+
+class HTTP_Response_Headers
+  {
+  private:
+    uint32_t m_status;
+    cow_string m_reason;
+    cow_bivector<cow_string, HTTP_Value> m_headers;
+
+  public:
+    // Constructors
+    explicit
+    HTTP_Response_Headers(uint32_t status) noexcept
+      : m_status(status)
+      { }
+
+    explicit
+    HTTP_Response_Headers(uint32_t status, cow_stringR reason) noexcept
+      : m_status(status), m_reason(reason)
+      { }
+
+    HTTP_Response_Headers&
+    swap(HTTP_Response_Headers& other) noexcept
+      {
+        ::std::swap(this->m_status, other.m_status);
+        this->m_reason.swap(other.m_reason);
+        this->m_headers.swap(other.m_headers);
+        return *this;
+      }
+
+  public:
+    ASTERIA_COPYABLE_DESTRUCTOR(HTTP_Response_Headers);
+
+    // Accesses raw data.
+    uint32_t
+    status() const noexcept
+      { return this->m_status;  }
+
+    void
+    set_status(uint32_t status) noexcept
+      { this->m_status = status;  }
+
+    cow_stringR
+    reason() const noexcept
+      { return this->m_reason;  }
+
+    void
+    set_reason(cow_stringR reason) noexcept
+      { this->m_reason = reason;  }
+
+    const cow_bivector<cow_string, HTTP_Value>&
+    headers() const noexcept
+      { return this->m_headers;  }
+
+    cow_bivector<cow_string, HTTP_Value>&
+    mut_headers() noexcept
+      { return this->m_headers;  }
+
+    size_t
+    count_headers() const noexcept
+      { return this->m_headers.size();  }
+
+    void
+    clear_headers() noexcept
+      { this->m_headers.clear();  }
+
+    void
+    resize_headers(size_t count)
+      { this->m_headers.resize(count);  }
+
+    cow_stringR
+    header_name(size_t index) const
+      { return this->m_headers.at(index).first;  }
+
+    const HTTP_Value&
+    header_value(size_t index) const
+      { return this->m_headers.at(index).second;  }
+
+    template<typename ValueT>
+    void
+    set_header(size_t index, cow_stringR name, ValueT&& value)
+      {
+        this->m_headers.mut(index).first = name;
+        this->m_headers.mut(index).second = ::std::forward<ValueT>(value);
+      }
+
+    template<typename ValueT>
+    void
+    add_header(cow_stringR name, ValueT&& value)
+      {
+        this->m_headers.emplace_back(name, ::std::forward<ValueT>(value));
+      }
+
+    // Writes response headers in raw format, which can be sent through a
+    // stream socket. Lines are separated by CR LF pairs.
+    tinyfmt&
+    print(tinyfmt& fmt) const;
+
+    cow_string
+    print_to_string() const;
+  };
+
+inline
+void
+swap(HTTP_Response_Headers& lhs, HTTP_Response_Headers& rhs) noexcept
+  {
+    lhs.swap(rhs);
+  }
+
+inline
+tinyfmt&
+operator<<(tinyfmt& fmt, const HTTP_Response_Headers& resp)
+  {
+    return resp.print(fmt);
+  }
+
+}  // namespace poseidon
+#endif
