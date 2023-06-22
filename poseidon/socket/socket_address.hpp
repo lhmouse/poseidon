3d312c412c8c16bb452e89d5621db7af0205107c
LIU Hao <lh_mouse@126.com>
2023-06-22 23:43:15 +0800

    socket_address: Cleanup

--- poseidon/socket/socket_address.hpp
+++ poseidon/socket/socket_address.hpp
@@ -130,13 +130,13 @@ class Socket_Address
     print_to_string() const;
   };
 
-extern const Socket_Address ipv6_unspecified;  // ::
-extern const Socket_Address ipv6_loopback;     // ::1
-extern const Socket_Address ipv6_invalid;      // 100::
+extern const Socket_Address ipv6_unspecified;  // [::]:0
+extern const Socket_Address ipv6_loopback;     // [::1]:0
+extern const Socket_Address ipv6_invalid;      // a documentation-only address
 
-extern const Socket_Address ipv4_unspecified;  // ::ffff::0.0.0.0
-extern const Socket_Address ipv4_loopback;     // ::ffff::127.0.0.1
-extern const Socket_Address ipv4_broadcast;    // ::ffff::255.255.255.255
+extern const Socket_Address ipv4_unspecified;  // [::ffff:0.0.0.0]:0
+extern const Socket_Address ipv4_loopback;     // [::ffff:127.0.0.1]:0
+extern const Socket_Address ipv4_broadcast;    // [::ffff:255.255.255.255]:0
 
 inline
 bool
