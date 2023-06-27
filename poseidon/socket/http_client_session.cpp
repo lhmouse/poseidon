d3be3197c9e66a4fa7aede4a8036cafd70187ad3
LIU Hao <lh_mouse@126.com>
2023-06-27 17:54:48 +0800

    http_client_session: New

--- poseidon/socket/http_client_session.cpp
+++ poseidon/socket/http_client_session.cpp
@@ -3,12 +3,337 @@
 
 #include "../precompiled.ipp"
 #include "http_client_session.hpp"
+#include "base/config_file.hpp"
+#include "static/main_config.hpp"
 #include "../utils.hpp"
 namespace poseidon {
 
 HTTP_Client_Session::
+HTTP_Client_Session()
+  : TCP_Socket()  // client constructor
+  {
+    ::http_parser_init(this->m_parser, HTTP_RESPONSE);
+    this->m_parser->data = this;
+  }
+
+HTTP_Client_Session::
 ~HTTP_Client_Session()
   {
+  }
+
+void
+HTTP_Client_Session::
+do_http_parser_on_message_begin()
+  {
+    this->m_resp.status = 0;
+    this->m_resp.reason.clear();
+    this->m_resp.headers.clear();
+    this->m_body.clear();
+  }
+
+void
+HTTP_Client_Session::
+do_http_parser_on_status(uint32_t status, const char* str, size_t len)
+  {
+    this->m_resp.status = status;
+
+    if(this->m_resp.reason.empty()) {
+      // The reason string is very likely a predefined one, so don't bother
+      // allocating storage for it.
+      auto static_reason = sref(::http_status_str((::http_status) this->m_resp.status));
+      if((static_reason.length() == len) && (::memcmp(static_reason.c_str(), str, len) == 0))
+        this->m_resp.reason = static_reason;
+      else
+        this->m_resp.reason.assign(str, len);
+    }
+    else
+      this->m_resp.reason.append(str, len);
+  }
+
+void
+HTTP_Client_Session::
+do_http_parser_on_header_field(const char* str, size_t len)
+  {
+    if(this->m_resp.headers.empty())
+      this->m_resp.headers.emplace_back();
+
+    if(this->m_body.size() != 0) {
+      // If `m_body` is not empty, a previous header will now be complete, so
+      // commit it.
+      const char* value_str = this->m_body.data() + 1;
+      ROCKET_ASSERT(value_str[-1] == '\n');  // magic character
+      size_t value_len = this->m_body.size() - 1;
+
+      auto& value = this->m_resp.headers.mut_back().second;
+      if(value.parse(value_str, value_len) != value_len)
+        value.set_string(cow_string(value_str, value_len));
+
+      // Create the next header.
+      this->m_resp.headers.emplace_back();
+      this->m_body.clear();
+    }
+
+    // Append the header name to the last key, as this callback might
+    // be invoked repeatedly.
+    this->m_resp.headers.mut_back().first.append(str, len);
+  }
+
+void
+HTTP_Client_Session::
+do_http_parser_on_header_value(const char* str, size_t len)
+  {
+    // Add a magic character to indicate that a part of the value has been
+    // received. This makes `m_body` non-empty.
+    if(this->m_body.empty())
+       this->m_body.putc('\n');
+
+    // Abuse `m_body` to store the header value.
+    this->m_body.putn(str, len);
+  }
+
+HTTP_Message_Body_Type
+HTTP_Client_Session::
+do_http_parser_on_headers_complete()
+  {
+    if(this->m_body.size() != 0) {
+      // If `m_body` is not empty, a previous header will now be complete, so
+      // commit it.
+      const char* value_str = this->m_body.data() + 1;
+      ROCKET_ASSERT(value_str[-1] == '\n');  // magic character
+      size_t value_len = this->m_body.size() - 1;
+
+      auto& value = this->m_resp.headers.mut_back().second;
+      if(value.parse(value_str, value_len) != value_len)
+        value.set_string(cow_string(value_str, value_len));
+
+      // Finish abuse of `m_body`.
+      this->m_body.clear();
+    }
+
+    // The headers are complete, so determine whether we want a response body.
+    return this->do_on_http_response_headers(this->m_resp);
+  }
+
+void
+HTTP_Client_Session::
+do_http_parser_on_body(const char* str, size_t len)
+  {
+    this->m_body.putn(str, len);
+    this->do_on_http_response_body_stream(this->m_body);
+  }
+
+void
+HTTP_Client_Session::
+do_http_parser_on_message_complete()
+  {
+    this->do_on_http_response_finish(::std::move(this->m_resp), ::std::move(this->m_body));
+  }
+
+void
+HTTP_Client_Session::
+do_on_tcp_stream(linear_buffer& data, bool eof)
+  {
+    static constexpr ::http_parser_settings settings[1] =
+      {{
+        // on_message_begin
+        +[](::http_parser* ps)
+          {
+            const auto xthis = (HTTP_Client_Session*) ps->data;
+            xthis->do_http_parser_on_message_begin();
+            return 0;
+          },
+
+        // on_url
+        nullptr,
+
+        // on_status
+        +[](::http_parser* ps, const char* str, size_t len)
+          {
+            const auto xthis = (HTTP_Client_Session*) ps->data;
+            xthis->do_http_parser_on_status(ps->status_code, str, len);
+            return 0;
+          },
+
+        // on_header_field
+        +[](::http_parser* ps, const char* str, size_t len)
+          {
+            const auto xthis = (HTTP_Client_Session*) ps->data;
+            xthis->do_http_parser_on_header_field(str, len);
+            return 0;
+          },
+
+        // on_header_value
+        +[](::http_parser* ps, const char* str, size_t len)
+          {
+            const auto xthis = (HTTP_Client_Session*) ps->data;
+            xthis->do_http_parser_on_header_value(str, len);
+            return 0;
+          },
+
+        // on_headers_complete
+        +[](::http_parser* ps)
+          {
+            const auto xthis = (HTTP_Client_Session*) ps->data;
+            auto body_type = xthis->do_http_parser_on_headers_complete();
+            switch(body_type) {
+              case http_message_body_normal:
+                return 0;
+
+              case http_message_body_empty:
+                return 1;
+
+              case http_message_body_upgrade:
+                return 2;
+
+              default:
+                POSEIDON_THROW((
+                    "Invalid body type `$3` returned from `do_http_parser_on_headers_complete()`",
+                    "[HTTP client `$1` (class `$2`)]"),
+                    xthis, typeid(*xthis), body_type);
+            }
+          },
+
+        // on_body
+        +[](::http_parser* ps, const char* str, size_t len)
+          {
+            const auto xthis = (HTTP_Client_Session*) ps->data;
+            xthis->do_http_parser_on_body(str, len);
+            return 0;
+          },
+
+        // on_message_complete
+        +[](::http_parser* ps)
+          {
+            const auto xthis = (HTTP_Client_Session*) ps->data;
+            xthis->do_http_parser_on_message_complete();
+            return 0;
+          },
+
+        // on_chunk_header
+        nullptr,
+
+        // on_chunk_complete
+        nullptr,
+      }};
+
+    // Parse incoming data and remove parsed bytes from the queue. Errors are
+    // are passed via exceptions.
+    if(data.size() > 0)
+      data.discard(::http_parser_execute(this->m_parser, settings, data.data(), data.size()));
+
+    if(eof)
+      ::http_parser_execute(this->m_parser, settings, "", 0);
+
+    // Assuming the error code won't be clobbered by the second call for EOF
+    // above. Not sure.
+    if(HTTP_PARSER_ERRNO(this->m_parser) != HPE_OK)
+      POSEIDON_THROW((
+          "HTTP parser error `$3`: $4",
+          "[HTTP client `$1` (class `$2`)]"),
+          this, typeid(*this), HTTP_PARSER_ERRNO(this->m_parser),
+          ::http_errno_description(HTTP_PARSER_ERRNO(this->m_parser)));
+  }
+
+HTTP_Message_Body_Type
+HTTP_Client_Session::
+do_on_http_response_headers(HTTP_Response_Headers& resp)
+  {
+    POSEIDON_LOG_INFO((
+        "HTTP client received response: $3 $4",
+        "[HTTP client `$1` (class `$2`)]"),
+        this, typeid(*this), resp.status, resp.reason);
+
+    // The default handler doesn't handle HEAD, CONNECT or Upgrade requests.
+    return http_message_body_normal;
+  }
+
+void
+HTTP_Client_Session::
+do_on_http_response_body_stream(linear_buffer& data)
+  {
+    // Leave `data` alone for consumption by `do_on_http_response_finish()`,
+    // but perform some safety checks, so we won't be affected by compromized
+    // 3rd-party servers.
+    const auto conf_file = main_config.copy();
+    int64_t max_response_content_length = 1048576;
+
+    auto value = conf_file.query("network", "http", "max_response_content_length");
+    if(value.is_integer())
+      max_response_content_length = value.as_integer();
+    else if(!value.is_null())
+      POSEIDON_LOG_WARN((
+          "Ignoring `network.http.max_response_content_length`: expecting an `integer`, got `$1`",
+          "[in configuration file '$2']"),
+          value, conf_file.path());
+
+    if(max_response_content_length < 0)
+      POSEIDON_THROW((
+          "`network.http.max_response_content_length` value `$1` out of range",
+          "[in configuration file '$2']"),
+          max_response_content_length, conf_file.path());
+
+    if(data.size() > (uint64_t) max_response_content_length)
+       POSEIDON_THROW((
+          "HTTP response body too large: `$3` > `$4`",
+          "[HTTP client `$1` (class `$2`)]"),
+          this, typeid(*this), data.size(), max_response_content_length);
+  }
+
+bool
+HTTP_Client_Session::
+http_request(HTTP_Request_Headers&& resp, const char* data, size_t size)
+  {
+    // Erase bad headers.
+    for(size_t hindex = 0;  hindex < resp.headers.size();  hindex ++)
+      if(resp.header_name_equals(hindex, sref("Content-Length"))
+          || resp.header_name_equals(hindex, sref("Transfer-Encoding")))
+        resp.headers.erase(hindex --);
+
+    if(size != 0) {
+      // By default, request messages do not have bodies. Hence the length is
+      // only necessary if the body is non-empty.
+      resp.headers.emplace_back(sref("Content-Length"), (int64_t) size);
+    }
+
+    // Compose the message and send it as a whole.
+    ::rocket::tinyfmt_str fmt;
+    fmt << resp;
+    fmt.putn(data, size);
+    return this->tcp_send(fmt.c_str(), fmt.length());
+  }
+
+bool
+HTTP_Client_Session::
+http_chunked_request_start(HTTP_Request_Headers&& resp)
+  {
+    // Erase bad headers.
+    for(size_t hindex = 0;  hindex < resp.headers.size();  hindex ++)
+      if(resp.header_name_equals(hindex, sref("Transfer-Encoding")))
+        resp.headers.erase(hindex --);
+
+    // Write a chunked header.
+    resp.headers.emplace_back(sref("Transfer-Encoding"), sref("chunked"));
+
+    // Compose the message header and send it as a whole.
+    ::rocket::tinyfmt_str fmt;
+    fmt << resp;
+    return this->tcp_send(fmt.c_str(), fmt.length());
+  }
+
+bool
+HTTP_Client_Session::
+http_chunked_request_data(const char* data, size_t size)
+  {
+    // Compose a chunk header and send it as a whole. The length of this chunk
+    // is written as a hexadecimal integer without the `0x` prefix.
+    ::rocket::tinyfmt_str fmt;
+    ::rocket::ascii_numput nump;
+    nump.put_XU(size);
+    fmt.putn(nump.data() + 2, nump.size() - 2);
+    fmt << "\r\n";
+    fmt.putn(data, size);
+    fmt << "\r\n";
+    return this->tcp_send(fmt.c_str(), fmt.length());
   }
 
 }  // namespace poseidon
--- poseidon/socket/http_client_session.hpp
+++ poseidon/socket/http_client_session.hpp
@@ -6,6 +6,9 @@
 
 #include "../fwd.hpp"
 #include "tcp_socket.hpp"
+#include "../http/http_request_headers.hpp"
+#include "../http/http_response_headers.hpp"
+#include <http_parser.h>
 namespace poseidon {
 
 class HTTP_Client_Session
@@ -14,9 +17,111 @@ class HTTP_Client_Session
   private:
     friend class Network_Driver;
 
+    ::http_parser m_parser[1];
+    HTTP_Response_Headers m_resp;
+    linear_buffer m_body;
+
+  public:
+    // Constructs a socket for outgoing connections.
+    explicit
+    HTTP_Client_Session();
+
+  private:
+    inline
+    void
+    do_http_parser_on_message_begin();
+
+    inline
+    void
+    do_http_parser_on_status(uint32_t status, const char* str, size_t len);
+
+    inline
+    void
+    do_http_parser_on_header_field(const char* str, size_t len);
+
+    inline
+    void
+    do_http_parser_on_header_value(const char* str, size_t len);
+
+    inline
+    HTTP_Message_Body_Type
+    do_http_parser_on_headers_complete();
+
+    inline
+    void
+    do_http_parser_on_body(const char* str, size_t len);
+
+    inline
+    void
+    do_http_parser_on_message_complete();
+
+  protected:
+    // This function implements `TCP_Socket`.
+    virtual
+    void
+    do_on_tcp_stream(linear_buffer& data, bool eof) override;
+
+    // This callback is invoked by the network thread after all headers of a
+    // response have been received, just before the body of it. Returning
+    // `http_message_body_normal` indicates that the response has a body whose
+    // length is described by the `Content-Length` or `Transfer-Encoding`
+    // header. Returning `http_message_body_empty` indicates that the message
+    // does not have a body even if it appears so, such as the response to a
+    // HEAD request. Returning `http_message_body_upgrade` causes all further
+    // data on this connection to be delivered via `do_on_http_body_stream()`.
+    // This callback is primarily used to examine the status code before
+    // processing response data.
+    // The default implementation does not check for HEAD or upgrade responses
+    // and returns `http_message_body_normal`.
+    virtual
+    HTTP_Message_Body_Type
+    do_on_http_response_headers(HTTP_Response_Headers& resp);
+
+    // This callback is invoked by the network thread for each fragment of the
+    // response body that has been received. As with `TCP_Connection::
+    // do_on_tcp_stream`, the argument buffer contains all data that have been
+    // accumulated so far and callees are supposed to remove bytes that have
+    // been processed.
+    // The default implementation leaves all data alone for consumption by
+    // `do_on_http_response_finish()`, but it checks the total length of the
+    // body so it will not exceed `network.http.max_response_content_length`
+    // in 'main.conf'.
+    virtual
+    void
+    do_on_http_response_body_stream(linear_buffer& data);
+
+    // This callback is invoked by the network thread at the end of a response
+    // message. Arguments have the same semantics with the other callbacks.
+    virtual
+    void
+    do_on_http_response_finish(HTTP_Response_Headers&& resp, linear_buffer&& data) = 0;
+
   public:
     ASTERIA_NONCOPYABLE_VIRTUAL_DESTRUCTOR(HTTP_Client_Session);
 
+    // Sends a simple request, possibly with a complete body. Callers should
+    // not supply `Content-Length` or `Transfer-Encoding` headers, as they
+    // will be rewritten.
+    // If this function throws an exception, there is no effect.
+    // This function is thread-safe.
+    bool
+    http_request(HTTP_Request_Headers&& resp, const char* data, size_t size);
+
+    // Initiates a request with a chunked body. Callers should not supply
+    // a `Transfer-Encoding` header, as it will be rewritten.
+    // If this function throws an exception, there is no effect.
+    // This function is thread-safe.
+    bool
+    http_chunked_request_start(HTTP_Request_Headers&& resp);
+
+    // Sends a chunk of the request body. The HTTP/1.1 specification says that
+    // a chunk of length zero terminates the chunked body. Chunked trailers
+    // have not yet been supported. This function shall only be called when a
+    // chunked request is active, otherwise it will corrupt the connection.
+    // If this function throws an exception, there is no effect.
+    // This function is thread-safe.
+    bool
+    http_chunked_request_data(const char* data, size_t size);
   };
 
 }  // namespace poseidon
