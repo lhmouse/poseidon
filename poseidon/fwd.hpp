2a2bf394fd463bed2f35483d8e36283a4e1f9517
LIU Hao <lh_mouse@126.com>
2023-07-06 18:53:07 +0800

    fwd: Reorder declarations

--- poseidon/fwd.hpp
+++ poseidon/fwd.hpp
@@ -252,12 +252,11 @@ class HTTPS_Client_Session;
 // HTTP types
 class HTTP_DateTime;
 class HTTP_Value;
-class HTTP_Header_Parser;
-
+using HTTP_Header_Vector = cow_bivector<cow_string, HTTP_Value>;
 struct HTTP_Request_Headers;
 struct HTTP_Response_Headers;
+class HTTP_Header_Parser;
 struct WebSocket_Frame_Header;
-using HTTP_Header_Vector = cow_bivector<cow_string, HTTP_Value>;
 
 // Easy types
 // Being 'easy' means all callbacks are invoked in fibers and can perform
