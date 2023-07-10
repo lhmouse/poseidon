cb5ff77e65a62f17e1fdae4bbabb1963df808263
LIU Hao <lh_mouse@126.com>
2023-07-10 18:12:27 +0800

    websocket_compositor: Add PMCE support

--- poseidon/http/websocket_compositor.cpp
+++ poseidon/http/websocket_compositor.cpp
@@ -16,17 +16,13 @@ namespace {
 
 struct Sec_WebSocket
   {
-    unsigned char key[16];
-    char key_str[25];
+    unsigned char key[16];  // MD5_DIGEST_LENGTH
+    char key_str[25] = "";  // ceil(sizeof(key) / 3) * 4 + 1
+    char key_padding[3];  // 44
 
-    unsigned char accept[20];
-    char accept_str[29];
-
-    Sec_WebSocket() noexcept
-      {
-        this->key_str[0] = 0;
-        this->accept_str[0] = 0;
-      }
+    unsigned char accept[20];  // SHA_DIGEST_LENGTH
+    char accept_str[29] = "";  // ceil(sizeof(accept) / 3) * 4 + 1
+    char accept_padding[3];  // 52
 
     void
     make_key_str(const void* self)
@@ -51,6 +47,86 @@ struct Sec_WebSocket
       }
   };
 
+struct PerMessage_Deflate
+  {
+    bool enabled = false;
+    bool server_no_context_takeover;
+    bool client_no_context_takeover;
+    int server_max_window_bits;
+    int client_max_window_bits;
+
+    void
+    use_permessage_deflate(HTTP_Header_Parser& hparser)
+      {
+        if(this->enabled)
+          return;
+
+        // Set default parameters, so in case of errors, we return immediately.
+        this->server_no_context_takeover = false;
+        this->client_no_context_takeover = false;
+        this->server_max_window_bits = 15;
+        this->client_max_window_bits = 15;
+
+        // PMCE is accepted only if all parameters are valid and accepted.
+        // Reference: https://datatracker.ietf.org/doc/html/rfc7692
+        while(hparser.next_attribute())
+          if(ascii_ci_equal(hparser.current_name(), sref("server_no_context_takeover"))) {
+            if(!hparser.current_value().is_null())
+              return;
+
+            // `server_no_context_takeover`:
+            // States that the server will not reuse a previous LZ77 sliding
+            // window when compressing a message. Ignored by clients.
+            this->server_no_context_takeover = true;
+          }
+          else if(ascii_ci_equal(hparser.current_name(), sref("client_no_context_takeover"))) {
+            if(!hparser.current_value().is_null())
+              return;
+
+            // `client_no_context_takeover`:
+            // States that the client will not reuse a previous LZ77 sliding
+            // window when compressing a message. Ignored by servers.
+            this->client_no_context_takeover = true;
+          }
+          else if(ascii_ci_equal(hparser.current_name(), sref("server_max_window_bits"))) {
+            if(hparser.current_value().is_null())
+              continue;
+            else if(!hparser.current_value().is_number())
+              return;
+
+            double value = hparser.current_value().as_number();
+            if(!(value >= 9) || !(value <= 15) || (value != (int) value))
+              return;
+
+            // `server_max_window_bits`:
+            // States the maximum size of the LZ77 sliding window that the server
+            // will use, in number of bits.
+            this->server_max_window_bits = (int) value;
+          }
+          else if(ascii_ci_equal(hparser.current_name(), sref("client_max_window_bits"))) {
+            if(hparser.current_value().is_null())
+              continue;
+            else if(!hparser.current_value().is_number())
+              return;
+
+            double value = hparser.current_value().as_number();
+            if(!(value >= 9) || !(value <= 15) || (value != (int) value))
+              return;
+
+            // `client_max_window_bits`:
+            // States the maximum size of the LZ77 sliding window that the client
+            // will use, in number of bits.
+            this->client_max_window_bits = (int) value;
+          }
+          else
+            return;
+
+        // If all parameters have been acepted, accept this PMCE specification.
+        // The caller shall initialize a deflator and inflator accordingly.
+        this->enabled = true;
+      }
+  };
+
 }  // namespace
 
 WebSocket_Compositor::
@@ -72,7 +148,7 @@ create_handshake_request(HTTP_Request_Headers& req)
     req.uri = sref("/");
     req.headers.clear();
     req.headers.reserve(8);
-    req.headers.emplace_back(sref("Connection"), sref("upgrade"));
+    req.headers.emplace_back(sref("Connection"), sref("Upgrade"));
     req.headers.emplace_back(sref("Upgrade"), sref("websocket"));
     req.headers.emplace_back(sref("Sec-WebSocket-Version"), 13);
 
@@ -110,6 +186,7 @@ accept_handshake_request(HTTP_Response_Headers& resp, const HTTP_Request_Headers
     bool upgrade_ok = false;
     bool ws_version_ok = false;
     Sec_WebSocket sec_ws;
+    PerMessage_Deflate pmce;
 
     for(const auto& hpair : req.headers)
       if(ascii_ci_equal(hpair.first, sref("Connection"))) {
@@ -146,6 +223,18 @@ accept_handshake_request(HTTP_Response_Headers& resp, const HTTP_Request_Headers
         if(hpair.second.as_string().length() == 24)
           ::memcpy(sec_ws.key_str, hpair.second.as_c_str(), 25);
       }
+      else if(ascii_ci_equal(hpair.first, sref("Sec-WebSocket-Extensions"))) {
+        if(!hpair.second.is_string())
+          continue;
+
+        // Sec-WebSocket-Extensions: permessage-deflate; client_max_window_bits
+        hparser.reload(hpair.second.as_string());
+        while(hparser.next_element())
+          if(ascii_ci_equal(hparser.current_name(), sref("permessage-deflate")))
+            pmce.use_permessage_deflate(hparser);
+          else
+            return;  // unknown extension; ignore
+      }
 
     if(!upgrade_ok || !ws_version_ok || !sec_ws.key_str[0]) {
       // Respond with `426 Upgrade Required`.
@@ -160,11 +249,27 @@ accept_handshake_request(HTTP_Response_Headers& resp, const HTTP_Request_Headers
     // Compose the response.
     resp.status = 101;
     resp.headers.clear();
-    resp.headers.emplace_back(sref("Connection"), sref("upgrade"));
+    resp.headers.emplace_back(sref("Connection"), sref("Upgrade"));
     resp.headers.emplace_back(sref("Upgrade"), sref("websocket"));
 
     sec_ws.make_accept_str();
     resp.headers.emplace_back(sref("Sec-WebSocket-Accept"), cow_string(sec_ws.accept_str, 28));
+
+    if(pmce.enabled) {
+      // Append PMCE response parameters. `client_no_context_takeover` and
+      // `client_max_window_bits` are ignored and unnecessary to be sent back to
+      // the client.
+      tinyfmt_str pmce_resp_fmt;
+      pmce_resp_fmt.set_string(sref("permessage-deflate"));
+
+      if(pmce.server_no_context_takeover)
+        pmce_resp_fmt << "; server_no_context_takeover";
+
+      if(pmce.server_max_window_bits != 15)
+        pmce_resp_fmt << "; server_max_window_bits=" << pmce.server_max_window_bits;
+
+      // Initialize data compressor and decompressor.
+    }
 
     // For the server, this connection has now been established.
     this->m_wsc = wsc_s_accepted;
@@ -187,6 +292,7 @@ accept_handshake_response(const HTTP_Response_Headers& resp)
 
     bool upgrade_ok = false;
     char sec_ws_accept_resp[29] = "";
+    PerMessage_Deflate pmce;
 
     for(const auto& hpair : resp.headers)
       if(ascii_ci_equal(hpair.first, sref("Connection"))) {
@@ -215,6 +321,18 @@ accept_handshake_response(const HTTP_Response_Headers& resp)
         if(hpair.second.as_string().length() == 28)
           ::memcpy(sec_ws_accept_resp, hpair.second.as_c_str(), 29);
       }
+      else if(ascii_ci_equal(hpair.first, sref("Sec-WebSocket-Extensions"))) {
+        if(!hpair.second.is_string())
+          continue;
+
+        // Sec-WebSocket-Extensions: permessage-deflate; client_max_window_bits
+        hparser.reload(hpair.second.as_string());
+        while(hparser.next_element())
+          if(ascii_ci_equal(hparser.current_name(), sref("permessage-deflate")))
+            pmce.use_permessage_deflate(hparser);
+          else
+            return;  // unknown extension; fail
+      }
 
     if(!upgrade_ok || !sec_ws_accept_resp[0]) {
       // Don't send a closure notification.
@@ -231,6 +349,10 @@ accept_handshake_response(const HTTP_Response_Headers& resp)
     // bits which shouldn't be examined, should they?
     if(::memcmp(sec_ws.accept_str, sec_ws_accept_resp, 28) != 0)
       return;
+
+    if(pmce.enabled) {
+      // Initialize data compressor and decompressor.
+    }
 
     // For the server, this connection has now been established.
     this->m_wsc = wsc_c_accepted;
