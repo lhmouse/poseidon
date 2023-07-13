bf8e8210482210f317566fcfad522eda86f948b2
LIU Hao <lh_mouse@126.com>
2023-07-13 17:58:30 +0800

    openssl_fwd: Refactor with macros

--- poseidon/third/openssl_fwd.hpp
+++ poseidon/third/openssl_fwd.hpp
@@ -5,130 +5,122 @@
 #define POSEIDON_THIRD_OPENSSL_FWD_
 
 #include "../fwd.hpp"
-#include <openssl/ossl_typ.h>
+#include <openssl/bio.h>
+#include <openssl/x509.h>
+#include <openssl/ssl.h>
 namespace poseidon {
 
-template<class SSLxT>
-struct openssl_ptr_traits;
-
-template<class SSLxT>
-class openssl_ptr
-  {
-  private:
-    SSLxT* m_ptr;
-
-  public:
-    constexpr
-    openssl_ptr() noexcept
-      : m_ptr(nullptr)
-      { }
-
-    constexpr
-    openssl_ptr(SSLxT* ptr) noexcept
-      : m_ptr(ptr)
-      { }
-
-    openssl_ptr(const openssl_ptr& other) noexcept
-      : m_ptr(other.do_up_ref())
-      { }
-
-    openssl_ptr(openssl_ptr&& other) noexcept
-      : m_ptr(other.release())
-      { }
-
-    openssl_ptr&
-    operator=(const openssl_ptr& other) & noexcept
-      { return this->reset(other.do_up_ref());  }
-
-    openssl_ptr&
-    operator=(openssl_ptr&& other) & noexcept
-      { return this->reset(other.release());  }
-
-    openssl_ptr&
-    swap(openssl_ptr& other) noexcept
-      {
-        ::std::swap(this->m_ptr, other.m_ptr);
-        return *this;
-      }
-
-    ~openssl_ptr()
-      { this->do_free();  }
-
-  private:
-    SSLxT*
-    do_up_ref() const noexcept
-      {
-        if(this->m_ptr == nullptr)
-          return nullptr;
-
-        int r = openssl_ptr_traits<SSLxT>::up_ref(this->m_ptr);
-        ROCKET_ASSERT(r == 1);
-        return this->m_ptr;
-      }
-
-    void
-    do_free() noexcept
-      {
-        if(this->m_ptr == nullptr)
-          return;
-
-        openssl_ptr_traits<SSLxT>::free(this->m_ptr);
-        this->m_ptr = (SSLxT*) -0xDEADBEEF;
-      }
-
-  public:
-    explicit constexpr operator
-    bool() const noexcept
-      { return this->m_ptr != nullptr;  }
-
-    constexpr operator
-    SSLxT*() const noexcept
-      { return this->m_ptr;  }
-
-    constexpr
-    SSLxT*
-    get() const noexcept
-      { return this->m_ptr;  }
-
-    openssl_ptr&
-    reset(SSLxT* ptr = nullptr) noexcept
-      {
-        this->do_free();
-        this->m_ptr = ptr;
-        return *this;
-      }
-
-    SSLxT*
-    release() noexcept
-      {
-        return ::std::exchange(this->m_ptr, nullptr);
-      }
-  };
-
-template<class SSLxT>
-inline
-void
-swap(openssl_ptr<SSLxT>& lhs, openssl_ptr<SSLxT>& rhs) noexcept
-  {
-    lhs.swap(rhs);
-  }
-
-#define POSEIDON_OPENSSL_FWD_DEFINE_PTR_(OBJ, xFree)  \
-  extern "C" int OBJ##_up_ref(::OBJ* a) __attribute__((__nothrow__));  \
-  extern "C" xFree OBJ##_free(::OBJ* a) __attribute__((__nothrow__));  \
-  template<>  \
-  struct openssl_ptr_traits<::OBJ>  \
+#define POSEIDON_OPENSSL_FWD_DEFINE_PTR_(OBJ)  \
+  \
+  class OBJ##_ptr  \
     {  \
-      static int up_ref(::OBJ* a) noexcept { return OBJ##_up_ref(a);  }  \
-      static void free(::OBJ* a) noexcept { OBJ##_free(a);  }  \
+    private:  \
+      OBJ* m_ptr;  \
+  \
+    public:  \
+      constexpr  \
+      OBJ##_ptr(nullptr_t = nullptr) noexcept  \
+        : m_ptr(nullptr)  \
+        { }  \
+  \
+      explicit constexpr  \
+      OBJ##_ptr(OBJ* ptr) noexcept  \
+        : m_ptr(ptr)  \
+        { }  \
+  \
+      OBJ##_ptr(const OBJ##_ptr& other) noexcept  \
+        {  \
+          if(other.m_ptr) OBJ##_up_ref(other.m_ptr);  \
+          this->m_ptr = other.m_ptr;  \
+        }  \
+  \
+      OBJ##_ptr(OBJ##_ptr&& other) noexcept  \
+        {  \
+          this->m_ptr = other.m_ptr;  \
+          other.m_ptr = nullptr;  \
+        }  \
+  \
+      OBJ##_ptr&  \
+      operator=(nullptr_t) & noexcept  \
+        {  \
+          this->reset();  \
+          return *this;  \
+        }  \
+  \
+      OBJ##_ptr&  \
+      operator=(const OBJ##_ptr& other) & noexcept  \
+        {  \
+          if(other.m_ptr) OBJ##_up_ref(other.m_ptr);  \
+          if(this->m_ptr) OBJ##_free(this->m_ptr);  \
+          this->m_ptr = other.m_ptr;  \
+          return *this;  \
+        }  \
+  \
+      OBJ##_ptr&  \
+      operator=(OBJ##_ptr&& other) & noexcept  \
+        {  \
+          ::std::swap(this->m_ptr, other.m_ptr);  \
+          return *this;  \
+        }  \
+  \
+      OBJ##_ptr&  \
+      swap(OBJ##_ptr& other) noexcept  \
+        {  \
+          ::std::swap(this->m_ptr, other.m_ptr);  \
+          return *this;  \
+        }  \
+  \
+      ~OBJ##_ptr()  \
+        {  \
+          if(this->m_ptr) OBJ##_free(this->m_ptr);  \
+          this->m_ptr = (OBJ*) -0xDEADBEEF;  \
+        }  \
+  \
+    public:  \
+      explicit constexpr operator  \
+      bool() const noexcept  \
+        { return this->m_ptr != nullptr;  }  \
+  \
+      constexpr operator  \
+      OBJ*() const noexcept  \
+        { return this->m_ptr;  }  \
+  \
+      constexpr  \
+      OBJ*  \
+      get() const noexcept  \
+        { return this->m_ptr;  }  \
+  \
+      OBJ##_ptr&  \
+      reset(OBJ* ptr = nullptr) noexcept  \
+        {  \
+          ROCKET_ASSERT(!ptr || (this->m_ptr != ptr));  \
+          if(this->m_ptr) OBJ##_free(this->m_ptr);  \
+          this->m_ptr = ptr;  \
+          return *this;  \
+        }  \
+  \
+      OBJ*  \
+      release() noexcept  \
+        {  \
+          OBJ* ptr = this->m_ptr;  \
+          this->m_ptr = nullptr;  \
+          return ptr;  \
+        }  \
     };  \
-  using OBJ##_ptr = openssl_ptr<::OBJ>;
+  \
+  inline  \
+  void  \
+  swap(OBJ##_ptr& lhs, OBJ##_ptr& rhs) noexcept  \
+    {  \
+      lhs.swap(rhs);  \
+    }  \
+  \
+  class OBJ##_ptr  // no semicolon
 
-POSEIDON_OPENSSL_FWD_DEFINE_PTR_(BIO, int)          // class BIO_ptr
-POSEIDON_OPENSSL_FWD_DEFINE_PTR_(X509_STORE, void)  // class X509_STORE_ptr
-POSEIDON_OPENSSL_FWD_DEFINE_PTR_(X509, void)        // class X509_ptr
-POSEIDON_OPENSSL_FWD_DEFINE_PTR_(SSL_CTX, void)     // class SSL_CTX_ptr
-POSEIDON_OPENSSL_FWD_DEFINE_PTR_(SSL, void)         // class SSL_ptr
+POSEIDON_OPENSSL_FWD_DEFINE_PTR_(BIO);      // class BIO_ptr
+POSEIDON_OPENSSL_FWD_DEFINE_PTR_(X509);     // class X509_ptr
+POSEIDON_OPENSSL_FWD_DEFINE_PTR_(SSL_CTX);  // class SSL_CTX_ptr
+POSEIDON_OPENSSL_FWD_DEFINE_PTR_(SSL);      // class SSL_ptr
 
 } // namespace poseidon
 #endif
