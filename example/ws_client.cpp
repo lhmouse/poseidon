552a79fe38f08638b90c2ded6b9fcafb27ab6d57
LIU Hao <lh_mouse@126.com>
2023-07-16 01:11:36 +0800

    example/ws_client: Use IPv4 loopback address

--- example/ws_client.cpp
+++ example/ws_client.cpp
@@ -49,7 +49,7 @@ timer_callback(shptrR<Abstract_Timer> /*timer*/, Abstract_Fiber& /*fiber*/, stea
 
     switch(state) {
       case 0: {
-        Socket_Address addr("[::1]:3806");
+        Socket_Address addr("127.0.0.1:3806");
         my_client.connect(addr);
         POSEIDON_LOG_INFO(("example WS client connecting: addr = $1"), addr);
         break;
