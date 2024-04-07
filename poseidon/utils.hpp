617a82f6a391fe04332715ca929fb633dd1accc5
LIU Hao <lh_mouse@126.com>
2024-04-07 18:36:37 +0800

    utils: Fix ADL
    Like in 125793a9394791a60f8f2e20a634c3708c4cd9a7.

--- poseidon/utils.hpp
+++ poseidon/utils.hpp
@@ -194,6 +194,7 @@ create_runtime_error(vfptr<cow_string&, void*> composer_thunk, void* composer,
 #define POSEIDON_THROW(TEMPLATE, ...)  \
     throw (__extension__  \
        ({  \
+         using ::asteria::format;  \
          auto IuChah0u = [&](::asteria::cow_string& quu1Opae)  \
            { format(quu1Opae, (::asteria::make_string_template TEMPLATE), ##__VA_ARGS__);  };  \
          auto ohng0Ohh = [](::asteria::cow_string& iughih5B, void* fi8OhNgo)  \
