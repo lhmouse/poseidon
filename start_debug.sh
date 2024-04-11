b527df94009cddba178680f563e6f883cab41fb6
LIU Hao <lh_mouse@126.com>
2024-04-11 13:31:21 +0800

    misc: Update start scripts

--- start_debug.sh
+++ start_debug.sh
@@ -1,9 +1,9 @@
 #!/bin/bash -e
 
-_san_so_list=$(ldd ./build_debug/libposeidon.so |
-               grep -Eho '\<lib(a|ub|t)san\.so.[0-9]+\>' |
-               sort -u)
+meson compile -Cbuild_debug
 
 export LD_LIBRARY_PATH=$(realpath -e build_debug)
-export LD_PRELOAD=$(echo ${_san_so_list})
+export LD_PRELOAD=$(ldd ./build_debug/libposeidon.so |
+                    grep -Eho '\<lib(a|ub|t)san\.so\>' |
+                    sort -u)
 ./build_debug/poseidon ./etc/poseidon
--- start_release.sh
+++ start_release.sh
@@ -1,4 +1,6 @@
 #!/bin/bash -e
 
+meson compile -Cbuild_release
+
 export LD_LIBRARY_PATH=$(realpath -e build_release)
 ./build_release/poseidon ./etc/poseidon
