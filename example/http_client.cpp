f0f0e87c7d851fe5126d1a9828bdca5957c03a97
LIU Hao <lh_mouse@126.com>
2024-04-17 14:18:49 +0800

    easy_http_client: Allow multiple connections

--- example/http_client.cpp
+++ example/http_client.cpp
@@ -7,12 +7,19 @@
 #include "../poseidon/utils.hpp"
 using namespace ::poseidon;
 
+static shptr<HTTP_Client_Session> my_client_session;
+
 static Easy_HTTP_Client my_client(
   // callback
   *[](shptrR<HTTP_Client_Session> session, Abstract_Fiber& fiber, Easy_HTTP_Event event,
       HTTP_Response_Headers&& resp, linear_buffer&& data)
   {
     (void) fiber;
+
+    if(event != easy_http_close)
+      my_client_session = session;
+    else
+      my_client_session = nullptr;
 
     switch(event)
       {
@@ -50,7 +57,7 @@ static Easy_Timer my_timer(
     (void) now;
     static uint32_t state;
 
-    if(my_client.session_opt() == nullptr)
+    if(my_client_session == nullptr)
       state = 0;
     else
       state ++;
@@ -68,9 +75,11 @@ static Easy_Timer my_timer(
       case 1:
         {
           HTTP_Request_Headers req;
+          ::strcpy(req.method, "GET");
           req.uri_path = &"/";
+          req.headers.emplace_back(&"Host", &"www.example.org");
           req.headers.emplace_back(&"Connection", &"keep-alive");
-          my_client.http_GET(move(req));
+          my_client_session->http_request(move(req), "");
           POSEIDON_LOG_ERROR(("example HTTP client: $1 $2"), req.method, req.uri_path);
         }
         break;
@@ -78,8 +87,10 @@ static Easy_Timer my_timer(
       case 2:
         {
           HTTP_Request_Headers req;
+          ::strcpy(req.method, "POST");
           req.uri_path = &"/";
-          my_client.http_POST(move(req), "testdata");
+          req.headers.emplace_back(&"Host", &"www.example.org");
+          my_client_session->http_request(move(req), "testdata");
           POSEIDON_LOG_ERROR(("example HTTP client: $1 $2"), req.method, req.uri_path);
         }
         break;
@@ -87,15 +98,17 @@ static Easy_Timer my_timer(
       case 3:
         {
           HTTP_Request_Headers req;
+          ::strcpy(req.method, "DELETE");
           req.uri_path = &"/";
-          my_client.http_DELETE(move(req));
+          req.headers.emplace_back(&"Host", &"www.example.org");
+          my_client_session->http_request(move(req), "");
           POSEIDON_LOG_ERROR(("example HTTP client: $1 $2"), req.method, req.uri_path);
         }
         break;
 
       default:
         POSEIDON_LOG_ERROR(("example HTTP client shutting down"));
-        my_client.close();
+        my_client_session->quick_close();
       }
   });
 
--- poseidon/easy/easy_http_client.hpp
+++ poseidon/easy/easy_http_client.hpp
@@ -25,10 +25,8 @@ class Easy_HTTP_Client
   private:
     thunk_type m_thunk;
 
-    shptr<DNS_Connect_Task> m_dns_task;
-    struct X_Event_Queue;
-    shptr<X_Event_Queue> m_queue;
-    shptr<HTTP_Client_Session> m_session;
+    struct X_Session_Table;
+    shptr<X_Session_Table> m_sessions;
 
   public:
     // Constructs a client. The argument shall be an invocable object taking
@@ -62,78 +60,9 @@ class Easy_HTTP_Client
     void
     connect(chars_view addr);
 
-    // Destroys the current connection without graceful shutdown. This function
-    // should only be called after all data from the server have been read and
-    // processed properly. Specifically, it is an error to close the connection
-    // as soon as a request with `Connection: close` has been sent; the client
-    // must always wait for the server to close the connection.
+    // Shuts down all connections.
     void
-    close() noexcept;
-
-    // Gets the connection session object.
-    shptrR<HTTP_Client_Session>
-    session_opt() const noexcept
-      { return this->m_session;  }
-
-    // Gets the local address of this HTTP client for incoming data. In case of
-    // errors, `ipv6_unspecified` is returned.
-    ROCKET_PURE
-    const IPv6_Address&
-    local_address() const noexcept;
-
-    // Gets the remote or connected address of this client for outgoing data. In
-    // case of errors, `ipv6_unspecified` is returned.
-    ROCKET_PURE
-    const IPv6_Address&
-    remote_address() const noexcept;
-
-    // Sends a GET request. The request cannot contain a payload body. The
-    // caller should neither set `req.method` nor supply `Content-Length` or
-    // `Transfer-Encoding` headers in `req.headers`, because they will be
-    // overwritten.
-    // If this function returns `true`, the request will have been enqueued;
-    // however it is not guaranteed to arrive at the server. If this function
-    // returns `false`, the connection will have been closed.
-    // If this function throws an exception, there is no effect.
-    // This function is thread-safe.
-    bool
-    http_GET(HTTP_Request_Headers&& req);
-
-    // Sends a POST request. The request must contain a payload body. The
-    // caller should neither set `req.method` nor supply `Content-Length` or
-    // `Transfer-Encoding` headers in `req.headers`, because they will be
-    // overwritten.
-    // If this function returns `true`, the request will have been enqueued;
-    // however it is not guaranteed to arrive at the server. If this function
-    // returns `false`, the connection will have been closed.
-    // If this function throws an exception, there is no effect.
-    // This function is thread-safe.
-    bool
-    http_POST(HTTP_Request_Headers&& req, chars_view data);
-
-    // Sends a PUT request. The request must contain a payload body. The
-    // caller should neither set `req.method` nor supply `Content-Length` or
-    // `Transfer-Encoding` headers in `req.headers`, because they will be
-    // overwritten.
-    // If this function returns `true`, the request will have been enqueued;
-    // however it is not guaranteed to arrive at the server. If this function
-    // returns `false`, the connection will have been closed.
-    // If this function throws an exception, there is no effect.
-    // This function is thread-safe.
-    bool
-    http_PUT(HTTP_Request_Headers&& req, chars_view data);
-
-    // Sends a DELETE request. The request cannot contain a payload body. The
-    // caller should neither set `req.method` nor supply `Content-Length` or
-    // `Transfer-Encoding` headers in `req.headers`, because they will be
-    // overwritten.
-    // If this function returns `true`, the request will have been enqueued;
-    // however it is not guaranteed to arrive at the server. If this function
-    // returns `false`, the connection will have been closed.
-    // If this function throws an exception, there is no effect.
-    // This function is thread-safe.
-    bool
-    http_DELETE(HTTP_Request_Headers&& req);
+    close_all() noexcept;
   };
 
 }  // namespace poseidon
