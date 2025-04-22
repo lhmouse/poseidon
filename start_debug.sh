d8cbcbb3254154525c66ecca4b8368377ad348d4
LIU Hao <lh_mouse@126.com>
2025-04-22 14:48:09 +0800

    build: Update sanitizer envs

--- start_debug.sh
+++ start_debug.sh
@@ -3,7 +3,7 @@
 meson compile -Cbuild_debug
 
 export LD_LIBRARY_PATH=$(realpath -e build_debug)
-export LD_PRELOAD=$(libtree ./build_debug/lib*.so |
-                    grep -Eho '\<lib(a|ub|t)san\.so\>' |
-                    sort -u)
+export LD_PRELOAD=$(libtree ./build_debug/lib*.so  \
+                    | grep -Eho '\<lib(a|ub|t)san\.so\.\d+\>'  \
+                    | sort -u)
 ./build_debug/poseidon ./etc/poseidon
