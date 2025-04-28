aa5a2c32d1299b651091861b6a4042a7bd1e6499
LIU Hao <lh_mouse@126.com>
2025-04-28 14:04:27 +0800

    build: Update scripts

--- start_debug.sh
+++ start_debug.sh
@@ -1,11 +1,6 @@
 #!/bin/bash -e
 
-if ! test -d build_debug
-then
-  rm -rf build_debug
-  meson setup -Ddebug=true -Doptimization=0 -Denable-debug-checks=true build_debug
-fi
-
+meson setup -Ddebug=true -Doptimization=0 -Denable-debug-checks=true build_debug
 meson compile -Cbuild_debug
 
 export LD_LIBRARY_PATH=$(realpath -e build_debug)
--- start_release.sh
+++ start_release.sh
@@ -1,11 +1,6 @@
 #!/bin/bash -e
 
-if ! test -d build_release
-then
-  rm -rf build_release
-  meson setup -Ddebug=true -Doptimization=3 build_release
-fi
-
+meson setup -Ddebug=true -Doptimization=3 build_release
 meson compile -Cbuild_release
 
 export LD_LIBRARY_PATH=$(realpath -e build_release)
