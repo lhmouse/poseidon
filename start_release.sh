ca473e5939bc679b1de63f361e5bc0be9d6d1e8f
LIU Hao <lh_mouse@126.com>
2025-04-28 14:02:29 +0800

    build: Update scripts

--- start_release.sh
+++ start_release.sh
@@ -1,5 +1,11 @@
 #!/bin/bash -e
 
+if ! test -d build_release
+then
+  rm -rf build_release
+  meson setup -Ddebug=true -Doptimization=3 build_release
+fi
+
 meson compile -Cbuild_release
 
 export LD_LIBRARY_PATH=$(realpath -e build_release)
