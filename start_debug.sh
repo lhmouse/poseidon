e462e5b3a98019e1df385ff36f078922de19f3a6
LIU Hao <lh_mouse@126.com>
2025-05-19 15:18:03 +0800

    build: Update scripts

--- start_debug.sh
+++ start_debug.sh
@@ -4,4 +4,4 @@ meson setup -Ddebug=true -Doptimization=0 -Denable-debug-checks=true build_debug
 meson compile -Cbuild_debug
 
 export LD_LIBRARY_PATH=$(realpath -e build_debug)
-valgrind -- ./build_debug/poseidon ./etc/poseidon
+valgrind -- ./build_debug/poseidon $* ./etc/poseidon
