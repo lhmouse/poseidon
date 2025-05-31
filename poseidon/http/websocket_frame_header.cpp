b570a7b69332d09510bb9c6025279830bdc6a51f
LIU Hao <lh_mouse@126.com>
2025-06-01 01:30:52 +0800

    http/websocket_frame_header: Cleanup

--- poseidon/http/websocket_frame_header.cpp
+++ poseidon/http/websocket_frame_header.cpp
@@ -12,31 +12,47 @@ encode(tinyfmt& fmt) const
   {
     // Write the opcode byte. Fields are written according to the figure in
     // 5.2. Base Framing Protocol, RFC 6455, in big-endian byte order.
-    int ch = this->fin << 7 | this->rsv1 << 6 | this->rsv2 << 5 | this->rsv3 << 4 | this->opcode;
-    fmt.putc((char) ch);
+    char bytes[16];
+    uint32_t len = 0;
+
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wconversion"
+#pragma GCC diagnostic ignored "-Wsign-conversion"
+    bytes[len++] = this->fin << 7 | this->rsv1 << 6 | this->rsv2 << 5
+                   | this->rsv3 << 4 | this->opcode;
 
     if(this->payload_len <= 125) {
       // one-byte length
-      ch = this->mask << 7 | (int) this->payload_len;
-      fmt.putc((char) ch);
+      bytes[len++] = this->mask << 7 | this->payload_len;
     }
     else if(this->payload_len <= 65535) {
       // two-byte length
-      ch = this->mask << 7 | 126;
-      fmt.putc((char) ch);
-      uint16_t belen = ROCKET_HTOBE16((uint16_t) this->payload_len);
-      fmt.putn((const char*) &belen, 2);
+      bytes[len++] = this->mask << 7 | 126;
+      bytes[len++] = this->payload_len >> 8;
+      bytes[len++] = this->payload_len;
     }
     else {
       // eight-byte length
-      ch = this->mask << 7 | 127;
-      fmt.putc((char) ch);
-      uint64_t belen = ROCKET_HTOBE64(this->payload_len);
-      fmt.putn((const char*) &belen, 8);
+      bytes[len++] = this->mask << 7 | 127;
+      bytes[len++] = this->payload_len >> 56;
+      bytes[len++] = this->payload_len >> 48;
+      bytes[len++] = this->payload_len >> 40;
+      bytes[len++] = this->payload_len >> 32;
+      bytes[len++] = this->payload_len >> 24;
+      bytes[len++] = this->payload_len >> 16;
+      bytes[len++] = this->payload_len >> 8;
+      bytes[len++] = this->payload_len;
     }
 
-    if(this->mask)
-      fmt.putn((const char*) &(this->mask_key), 4);
+    if(this->mask) {
+      bytes[len++] = this->mask_key;
+      bytes[len++] = this->mask_key >> 8;
+      bytes[len++] = this->mask_key >> 16;
+      bytes[len++] = this->mask_key >> 24;
+    }
+#pragma GCC diagnostic pop
+
+    fmt.putn(bytes, len);
   }
 
 void
@@ -48,21 +64,25 @@ mask_payload(char* data, size_t size) noexcept
 
     char* cur = data;
     char* esdata = data + size;
-    uint32_t mask = ROCKET_LETOH32(mask_key);
-    __m128i xmm_mask = _mm_set1_epi32((int) mask);
+    uint32_t dw_mask = this->mask_key;
+    __m128i xmm_mask = _mm_set1_epi32(static_cast<int>(this->mask_key));
 
     while(esdata - cur >= 16) {
-      // XMMWORD-wise
-      __m128i* xcur = (__m128i*) cur;
-      _mm_storeu_si128(xcur, _mm_xor_si128(xmm_mask, _mm_loadu_si128(xcur)));
+      __m128i* xmmp = reinterpret_cast<__m128i*>(cur);
+      _mm_storeu_si128(xmmp, _mm_xor_si128(xmm_mask, _mm_loadu_si128(xmmp)));
       cur += 16;
     }
 
-    while(cur != esdata) {
-      // bytewise
-      *cur = (char) (mask ^ (uint8_t) *cur);
+    while(esdata - cur >= 4) {
+      uint32_t* dp = reinterpret_cast<uint32_t*>(cur);
+      *dp = *dp ^ dw_mask;
+      cur += 4;
+    }
+
+    while(esdata != cur) {
+      *cur = static_cast<char>(static_cast<uint8_t>(*cur) ^ dw_mask);
+      dw_mask = dw_mask << 24 | dw_mask >> 8;
       cur ++;
-      mask = mask << 24 | mask >> 8;
     }
   }
 
