437bba257e1e67cb226fdfb0676ebcfe57f15a4c
LIU Hao <lh_mouse@126.com>
2025-06-16 15:00:25 +0800

    base/uuid: Remove libuuid dependency

--- poseidon/base/uuid.cpp
+++ poseidon/base/uuid.cpp
@@ -5,14 +5,6 @@
 #include "uuid.hpp"
 #include "../utils.hpp"
 namespace poseidon {
-namespace {
-
-constexpr UUID s_uuid_min = POSEIDON_UUID(00000000,0000,0000,0000,000000000000);
-constexpr UUID s_uuid_max = POSEIDON_UUID(ffffffff,ffff,ffff,ffff,ffffffffffff);
-
-atomic_relaxed<uint64_t> s_serial;
-
-}  // namespace
 
 UUID::
 UUID(chars_view str)
@@ -25,14 +17,16 @@ const UUID&
 UUID::
 min() noexcept
   {
-    return s_uuid_min;
+    static constexpr UUID s = POSEIDON_UUID(00000000,0000,0000,0000,000000000000);
+    return s;
   }
 
 const UUID&
 UUID::
 max() noexcept
   {
-    return s_uuid_max;
+    static constexpr UUID s = POSEIDON_UUID(ffffffff,ffff,ffff,ffff,ffffffffffff);
+    return s;
   }
 
 UUID
@@ -47,21 +41,27 @@ random() noexcept
     ::clock_gettime(CLOCK_REALTIME, &ts);
     uint64_t high = static_cast<uint64_t>(ts.tv_sec - 983404800) * 30518 << 16;
     high += static_cast<uint32_t>(ts.tv_nsec) / 32768 << 16;
+
+    static atomic_relaxed<uint64_t> s_serial;
     high += s_serial.xadd(1) << 16;
 
-    // Then, set the `M` field, which is always `4` (UUID version 4).
+    // Set the `M` field, which is always `4` (UUID version 4).
     high |= 0x4000;
 
-    // Then, set the `yyy` field, which is the PID of the current process,
-    // truncated to 12 bits.
+    // Set the `yyy` field, which is the PID of the current process, truncated
+    // to 12 bits.
     high |= static_cast<uint32_t>(::getpid()) & 0x0FFF;
 
-    // Finally, generate the `Nzzz-zzzzzzzzzzzz` part with libuuid.
-    UUID st;
-    ::uuid_generate_random(st.m_uuid);
-    high = ROCKET_HTOBE64(high);
-    ::memcpy(st.m_uuid, &high, 8);
-    return st;
+    // Generate the `Nzzz-zzzzzzzzzzzz` part.
+    uint64_t low;
+    random_bytes(&low, 4);
+    low >>= 1;
+
+    // Compose the UUID.
+    UUID result;
+    result.m_high = ROCKET_HTOBE64(high);
+    result.m_low = ROCKET_HTOBE64(low);
+    return result;
   }
 
 int
@@ -82,28 +82,69 @@ size_t
 UUID::
 parse_partial(const char* str) noexcept
   {
-    int r = ::uuid_parse_range(str, str + 36, this->m_uuid);
-    ROCKET_ASSERT((r == 0) || (r == -1));
-    return static_cast<uint32_t>(r & 36) ^ 36;
+    uint64_t high = 0;
+    uint64_t low = 0;
+
+    for(uint32_t k = 0;  k != 36;  ++k)
+      if((0b100001000010000100000000U >> k) & 1) {
+        // dash
+        if(str[k] != '-')
+          return 0;
+      }
+      else {
+        // hex digit
+        uint64_t val;
+        if((str[k] >= '0') && (str[k] <= '9'))
+          val = static_cast<unsigned char>(str[k] - '0');
+        else if((str[k] >= 'A') && (str[k] <= 'F'))
+          val = static_cast<unsigned char>(str[k] - 'A' + 10);
+        else if((str[k] >= 'a') && (str[k] <= 'f'))
+          val = static_cast<unsigned char>(str[k] - 'a' + 10);
+        else
+          return 0;
+
+        if(k < 19)
+          high = high << 4 | val;
+        else
+          low = low << 4 | val;
+      }
+
+    this->m_high = ROCKET_HTOBE64(high);
+    this->m_low = ROCKET_HTOBE64(low);
+    return 36;
   }
 
 size_t
 UUID::
 parse(chars_view str) noexcept
   {
-    if(str.n != 36)
-      return 0;
+    if(str.n >= 36)
+      if(size_t aclen = this->parse_partial(str.p))
+        return aclen;
 
-    int r = ::uuid_parse_range(str.p, str.p + 36, this->m_uuid);
-    ROCKET_ASSERT((r == 0) || (r == -1));
-    return static_cast<uint32_t>(r & 36) ^ 36;
+    return 0;
   }
 
 size_t
 UUID::
 print_partial(char* str) const noexcept
   {
-    ::uuid_unparse_lower(this->m_uuid, str);
+    //            0123456789012345678901234567890123456
+    // * str    : xxxxxxxx-xxxx-Myyy-Nzzz-zzzzzzzzzzzz.
+    // * hexstr : xxxxxxxxxxxxMyyyNzzzzzzzzzzzzzzz.
+    char hexstr[33];
+    hex_encode_16_partial(hexstr, this->m_uuid);
+
+    str[36] = 0;
+    ::memcpy(str + 20, hexstr + 16, 16);
+    str[23] = '-';
+    ::memcpy(str + 19, hexstr + 16,  4);
+    str[18] = '-';
+    ::memcpy(str + 14, hexstr + 12,  4);
+    str[13] = '-';
+    ::memcpy(str +  9, hexstr +  8,  4);
+    str[ 8] = '-';
+    ::memcpy(str +  0, hexstr +  0,  8);
     return 36;
   }
 
