780f538ae9a3951d813115a98846522374e9df6a
LIU Hao <lh_mouse@126.com>
2025-07-08 17:13:11 +0800

    static/fiber_scheduler: Use new config file accessors

--- poseidon/static/fiber_scheduler.cpp
+++ poseidon/static/fiber_scheduler.cpp
@@ -240,81 +240,33 @@ void
 Fiber_Scheduler::
 reload(const Config_File& conf_file)
   {
-    // Parse new configuration. Default ones are defined here.
-    int64_t stack_vm_size = 0;
-    int64_t warn_timeout = 15;
-    int64_t fail_timeout = 300;
-
-    // Read the stack size from configuration.
-    auto conf_value = conf_file.query(&"fiber.stack_vm_size");
-    if(conf_value.is_integer())
-      stack_vm_size = conf_value.as_integer();
-    else if(!conf_value.is_null())
-      POSEIDON_THROW((
-          "Invalid `fiber.stack_vm_size`: expecting an `integer`, got `$1`",
-          "[in configuration file '$2']"),
-          conf_value, conf_file.path());
+    // Read the stack size from configuration. If no value or zero is specified,
+    // use the system's stack size.
+    auto vint = conf_file.get_integer_opt(&"fiber.stack_vm_size", 0, 16777216);
+    uint32_t stack_vm_size = static_cast<uint32_t>(vint.value_or(0));
 
     if(stack_vm_size == 0) {
-      // If no value or zero is specified, use the system's stack size.
+      // Use the system default value.
       ::rlimit rlim;
-      if(::getrlimit(RLIMIT_STACK, &rlim) != 0)
-        POSEIDON_THROW((
-            "Could not get system stack size",
-            "[`getrlimit()` failed: ${errno:full}]"));
-
-      // Hmmm this should be enough.
-      stack_vm_size = (uint32_t) rlim.rlim_cur;
+      if(::getrlimit(RLIMIT_STACK, &rlim) == 0)
+        stack_vm_size = static_cast<uint32_t>(rlim.rlim_cur);
     }
 
-    if((stack_vm_size < 0x10000) || (stack_vm_size > 0x7FFF0000))
-      POSEIDON_THROW((
-          "`fiber.stack_vm_size` value `$1` out of range",
-          "[in configuration file '$2']"),
-          stack_vm_size, conf_file.path());
+    if(stack_vm_size == 0)
+      stack_vm_size = 16777216;
 
-    if(stack_vm_size & 0xFFFF)
-      POSEIDON_THROW((
-          "`fiber.stack_vm_size` value `$1` not a multiple of 64KiB",
-          "[in configuration file '$2']"),
-          stack_vm_size, conf_file.path());
+    // Read fiber timeout values.
+    vint = conf_file.get_integer_opt(&"fiber.warn_timeout", 0, 86400);
+    seconds warn_timeout = seconds(static_cast<int>(vint.value_or(15)));
 
-    // Read scheduler timeouts inseconds.
-    conf_value = conf_file.query(&"fiber.warn_timeout");
-    if(conf_value.is_integer())
-      warn_timeout = conf_value.as_integer();
-    else if(!conf_value.is_null())
-      POSEIDON_THROW((
-          "Invalid `fiber.warn_timeout`: expecting an `integer`, got `$1`",
-          "[in configuration file '$2']"),
-          conf_value, conf_file.path());
-
-    if((warn_timeout < 0) || (warn_timeout > 86400))
-      POSEIDON_THROW((
-          "`fiber.warn_timeout` value `$1` out of range",
-          "[in configuration file '$2']"),
-          warn_timeout, conf_file.path());
-
-    conf_value = conf_file.query(&"fiber.fail_timeout");
-    if(conf_value.is_integer())
-      fail_timeout = conf_value.as_integer();
-    else if(!conf_value.is_null())
-      POSEIDON_THROW((
-          "Invalid `fiber.fail_timeout`: expecting an `integer`, got `$1`",
-          "[in configuration file '$2']"),
-          conf_value, conf_file.path());
-
-    if((fail_timeout < 0) || (fail_timeout > 86400))
-      POSEIDON_THROW((
-          "`fiber.fail_timeout` value `$1` out of range",
-          "[in configuration file '$2']"),
-          fail_timeout, conf_file.path());
+    vint = conf_file.get_integer_opt(&"fiber.fail_timeout", 0, 86400);
+    seconds fail_timeout = seconds(static_cast<int>(vint.value_or(300)));
 
     // Set up new data.
     plain_mutex::unique_lock lock(this->m_conf_mutex);
-    this->m_conf_stack_vm_size = static_cast<uint32_t>(stack_vm_size);
-    this->m_conf_warn_timeout = static_cast<seconds>(warn_timeout);
-    this->m_conf_fail_timeout = static_cast<seconds>(fail_timeout);
+    this->m_conf_stack_vm_size = stack_vm_size;
+    this->m_conf_warn_timeout = warn_timeout;
+    this->m_conf_fail_timeout = fail_timeout;
   }
 
 void
