b1e3839c2061e158e838ad011f835ff00333f64b
LIU Hao <lh_mouse@126.com>
2025-07-09 10:46:33 +0800

    base/config_file: Remove improper comments, copied from main_config

--- poseidon/base/config_file.hpp
+++ poseidon/base/config_file.hpp
@@ -68,49 +68,41 @@ class Config_File
 
     // Gets a boolean value. The value must exist and must be a boolean,
     // otherwise an exception is thrown.
-    // This function is thread-safe.
     bool
     get_boolean(chars_view vpath) const;
 
     // Gets a boolean value. If a non-null value exists, it must be a boolean,
     // otherwise an exception is thrown.
-    // This function is thread-safe.
     opt<bool>
     get_boolean_opt(chars_view vpath) const;
 
     // Gets an integer. The value must exist and must be an integer within the
     // given range, otherwise an exception is thrown.
-    // This function is thread-safe.
     int64_t
     get_integer(chars_view vpath, int64_t min, int64_t max) const;
 
     // Gets an integer. If a non-null value exists, it must be an integer
     // within the given range, otherwise an exception is thrown.
-    // This function is thread-safe.
     opt<int64_t>
     get_integer_opt(chars_view vpath, int64_t min, int64_t max) const;
 
     // Gets a float-point value. The value must exist and must be a number
     // within the given range, otherwise an exception is thrown.
-    // This function is thread-safe.
     double
     get_real(chars_view vpath, double min, double max) const;
 
     // Gets a float-point value. If a non-null value exists, it must be a
     // number within the given range, otherwise an exception is thrown.
-    // This function is thread-safe.
     opt<double>
     get_real_opt(chars_view vpath, double min, double max) const;
 
     // Gets a string. The value must exist and must be a string, otherwise an
     // exception is thrown.
-    // This function is thread-safe.
     const cow_string&
     get_string(chars_view vpath) const;
 
     // Gets a string. If a non-null value exists, it must be a string,
     // otherwise an exception is thrown.
-    // This function is thread-safe.
     opt<cow_string>
     get_string_opt(chars_view vpath) const;
   };
