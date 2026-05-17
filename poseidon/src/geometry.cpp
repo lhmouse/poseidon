6194cd54ab07e4a03889182baf4a6bfe5ec7778a
LIU Hao <lh_mouse@126.com>
2026-05-17 12:42:15 +0800

    geometry: Simplify `do_arctan_degrees()`

--- poseidon/src/geometry.cpp
+++ poseidon/src/geometry.cpp
@@ -419,14 +419,10 @@ do_arctan_degrees(float y, float x)
     ASTERIA_ASSERT((bits >= 0) && (bits <= 255));
     switch(bits)
       {
-      case 0b00001000 ... 0b00001111:
-      case 0b00011000 ... 0b00011111:
-      case 0b00111000 ... 0b01011111:
-      case 0b01110000 ... 0b01111111:
-      case 0b10001000 ... 0b10001111:
-      case 0b10011000 ... 0b10011111:
-      case 0b10111000 ... 0b11011111:
-      case 0b11110000 ... 0b11111111:
+      case 0b10001000:
+      case 0b10111100:
+      case 0b11000000:
+      case 0b11110000:
         // ((y != y) || (y == 0)) && (x >= 0)
         return 0;
 
@@ -438,18 +434,8 @@ do_arctan_degrees(float y, float x)
         // y > x > 0 > -x
         return 90 - do_reduced_arctan(x / y);
 
-      case 0b00100010 ... 0b00100011:
-      case 0b00100110 ... 0b00100111:
-      case 0b00101010 ... 0b00101011:
-      case 0b00101110 ... 0b00101111:
-      case 0b01100010 ... 0b01100011:
-      case 0b01100110 ... 0b01100111:
-      case 0b01101010 ... 0b01101011:
-      case 0b01101110 ... 0b01101111:
-      case 0b11100010 ... 0b11100011:
-      case 0b11100110 ... 0b11100111:
-      case 0b11101010 ... 0b11101011:
-      case 0b11101110 ... 0b11101111:
+      case 0b00100010:
+      case 0b11100111:
         // ((x != x) || (x == 0)) && (y > 0)
         return 90;
 
@@ -461,12 +447,10 @@ do_arctan_degrees(float y, float x)
         // -x > y > 0 > x
         return 180 - do_reduced_arctan(-y / x);
 
-      case 0b00000000 ... 0b00000111:
-      case 0b00010000 ... 0b00010111:
-      case 0b00110000 ... 0b00110111:
-      case 0b10000000 ... 0b10000111:
-      case 0b10010000 ... 0b10010111:
-      case 0b10110000 ... 0b10110111:
+      case 0b00000000:
+      case 0b00110000:
+      case 0b10000000:
+      case 0b10110001:
         // ((y != y) || (y == 0)) && (x < 0)
         return 180;
 
@@ -478,18 +462,8 @@ do_arctan_degrees(float y, float x)
         // -x > 0 > x > y
         return 270 - do_reduced_arctan(x / y);
 
-      case 0b00100000 ... 0b00100001:
-      case 0b00100100 ... 0b00100101:
-      case 0b00101000 ... 0b00101001:
-      case 0b00101100 ... 0b00101101:
-      case 0b01100000 ... 0b01100001:
-      case 0b01100100 ... 0b01100101:
-      case 0b01101000 ... 0b01101001:
-      case 0b01101100 ... 0b01101101:
-      case 0b11100000 ... 0b11100001:
-      case 0b11100100 ... 0b11100101:
-      case 0b11101000 ... 0b11101001:
-      case 0b11101100 ... 0b11101101:
+      case 0b00100000:
+      case 0b11100000:
         // ((x != x) || (x == 0)) && (y < 0)
         return 270;
 
