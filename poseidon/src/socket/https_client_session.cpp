87c35f91a572a259ae710334b327f55f757636b4
LIU Hao <lh_mouse@126.com>
2026-06-23 15:14:22 +0800

    https_client_session: Use `strlcpy()`

--- poseidon/src/socket/https_client_session.cpp
+++ poseidon/src/socket/https_client_session.cpp
@@ -14,10 +14,9 @@ HTTPS_Client_Session(const cow_string& default_host)
     Network_Reference caddr;
     size_t aclen = parse_network_reference(caddr, this->m_default_host);
     if((aclen != 0) && (caddr.host.n >= 1) && (caddr.host.n <= 253)) {
-      char str[256];
-      ::memcpy(str, caddr.host.p, caddr.host.n);
-      str[caddr.host.n] = 0;
-      ::SSL_set_tlsext_host_name(this->m_ssl, str);
+      charbuf_256 host_name;
+      ::strlcpy(host_name.mut_data(), caddr.host.p, caddr.host.n + 1);
+      ::SSL_set_tlsext_host_name(this->m_ssl, host_name.data());
     }
 
     static constexpr uint8_t s_alpn_protos[] = "\x08http/1.1";
